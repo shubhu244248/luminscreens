5954b1e5ed3e2f440ec3d924ca06307d3cf067d9
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-04-04 22:49:22 +0530

    Added colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,41 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-primary": "#000000",
+        "color-primary-light": "#0096ce",
+        "color-primary-dark": "#001064",
+        "color-secondary": "#00aeef",
+        "color-accent": "#017e01",
+        "color-gray": "#7f8d96",
+        "color-yellow": "#bd9503",
+        "color-light-gray": "#dadde2",
+        "color-lighter-gray": "#e7e7e7",
+        "color-danger": "#f20707",
+        "color-background": "#f6faf8",
+        "color-white": "#ffffff",
+        "color-deep-black": "#1f2020",
+        "color-dark-black": "#222222",
+        "color-darker-black": "#232323",
+        "color-black": "#262a31",
+        "color-medium-black": "#2b2c2e",
+        "color-light-black": "#2e3234",
+        "color-soft-black": "#333333",
+        "color-dim-gray": "#403f3f",
+        "color-steel-gray": "#575757",
+        "color-transparent": "rgba(0,0,0,0)",
+        "color-white-transparent-60": "rgba(255,255,255,0.6)",
+        "color-white-transparent-63": "rgba(255,255,255,0.63)",
+        "color-overlay": "#363636"
+      }
+
+    },
+    container: {
+      center: true
+    }
   },
+
   plugins: [],
 }
 
